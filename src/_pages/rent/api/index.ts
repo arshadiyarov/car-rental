327d425271b12920e56e5a58f610ccd557e99f6c
arshadiyarov <shadiyarovarmat@gmail.com>
2024-06-05 02:41:45 +0500

    fix: fetching functions

--- src/_pages/rent/api/index.ts
+++ src/_pages/rent/api/index.ts
@@ -2,13 +2,18 @@ import axios from "axios";
 import { Car } from "shared/common";
 
 export const getCars = async (): Promise<Car[]> => {
-  const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}`);
+  const res = await axios.get(
+    `https://665c3c1b3e4ac90a04d9021e.mockapi.io/cars`,
+  );
   return res.data;
 };
 
 export const putLike = async (id: string, liked: boolean): Promise<Car> => {
-  const res = await axios.put(`${process.env.NEXT_PUBLIC_API_URL}/${id}`, {
-    liked,
-  });
+  const res = await axios.put(
+    `https://665c3c1b3e4ac90a04d9021e.mockapi.io/cars/${id}`,
+    {
+      liked,
+    },
+  );
   return res.data;
 };
